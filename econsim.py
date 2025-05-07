135b74c095fcdc9eb4c513e69cc779ed5e9adb3c
Si Li <omikun@gmail.com>
2025-05-07 12:11:15 -0700

    basic life cycle works no trade yet

--- econsim.py
+++ econsim.py
@@ -3,38 +3,54 @@ import math
 import matplotlib.pyplot as plt
 
 # Initial populations
-agent_template = {'profession': 'none','hungry_steps': 0, 'food': 2, 'output': 0, 'input': 8}
+agent_template = {'profession': 'none','hungry_steps': 0, 'cash':10, 'inv': {}}
 num_agents = 20
 agents = [agent_template for _ in range(num_agents)]
 recipes = {}
 recipes['food'] = {'commodity': 'food', 'production': 4, 'price': 1, 'numInput': 0}
 recipes['wood'] = {'commodity': 'wood', 'production': 2, 'price': 2, 'numInput': 0}
 recipes['furniture'] = {'commodity': 'furniture', 'production': 1, 'input': 'wood', 'numInput': 8, 'price': 20}
-
 # Parameters
-time_steps = 3000
+time_steps = 4
 p_birth = .01
 starve_limit = 3
 
 food_pop = []
 wood_pop = []
 carp_pop = []
+foodInv = []
+woodInv = []
+carpInv = []
 
 #init
 for a in range(num_agents):
+    agent = agents[a]
+
     if a < 10:
-        agents[a]['profession'] = 'food'
+        agent['profession'] = 'food'
     if a < 18:
-        agents[a]['profession'] = 'wood'
+        agent['profession'] = 'wood'
     if a < 20:
-        agents[a]['profession'] = 'furniture'
+        agent['profession'] = 'furniture'
+
+    #init inventory
+    recipe = recipes[agent['profession']]
+    inputCom = recipe.get('input', 'none')
+    inv = agent['inv']
+    if inputCom != 'none':
+        inv[inputCom] = 10
+    outputCom = agent['profession']
+    inv[outputCom] = 0
+    inv['food'] = 2
+
 
 
 #main loop
 for t in range(time_steps):
     new_agents = []
     for agent in agents:
-        output = agents['profession']
+        output = agent['profession']
+        inv = agent['inv']
         recipe = recipes[output]
         #produce
         numOutput = 0
@@ -42,18 +58,25 @@ for t in range(time_steps):
             numOutput = recipe['production'] 
         else:
             com = recipe['input']
-            if agent[com] >= recipe['numInput']:
+            if inv[com] >= recipe['numInput']:
                 numOutput = recipe['production'] 
+                inv[com] -= recipe['numInput']
 
-        agent['output'] += numOutput
+        inv[output] += numOutput
+
+        #trade
+        #what if all trade are moneyless and communistic? take all food and redistribute
+        #take all wood and redistribute?
+        #take all furnitures and redistribute?
+
 
         #life cycle
-        if agent['food'] > 0:
-            food -= 1
+        if inv['food'] > 0:
+            inv['food'] -= 1
             agent['hungry_steps'] = 0
             if random.random() < p_birth:
-                new_wolves.append(agent_template)
-                new_wolves[-1]['profession'] = 'food'
+                new_agents.append(agent_template)
+                new_agents[-1]['profession'] = 'food'
         else:
             agent['hungry_steps'] += 1
         if agent['hungry_steps'] < starve_limit:
@@ -63,14 +86,20 @@ for t in range(time_steps):
 
 
     # Track population
-    food_pop.append(sum(agent['profession'] == 'food' for agent in agents)
-    wood_pop.append(sum(agent['profession'] == 'wood' for agent in agents)
-    carp_pop.append(sum(agent['profession'] == 'carp' for agent in agents)
+    foodInv.append(sum(agent['inv'].get('food', 0) for agent in agents))
+    woodInv.append(sum(agent['inv'].get('wood', 0) for agent in agents))
+    carpInv.append(sum(agent['inv'].get('furniture', 0) for agent in agents))
+    food_pop.append(sum(agent['profession'] == 'food' for agent in agents))
+    wood_pop.append(sum(agent['profession'] == 'wood' for agent in agents))
+    carp_pop.append(sum(agent['profession'] == 'furniture' for agent in agents))
 
 # Plot results
 figure, axis = plt.subplots(2, 1)
 figure.set_figwidth(10)
 figure.set_figheight(10)
+axis[1].plot(foodInv, label='FoodInv', color='green')
+axis[1].plot(woodInv, label='WoodInv', color='red')
+axis[1].plot(carpInv, label='carpInv', color='blue')
 axis[1].plot(food_pop, label='Food', color='green')
 axis[1].plot(wood_pop, label='Wood', color='red')
 axis[1].plot(carp_pop, label='carp', color='blue')
